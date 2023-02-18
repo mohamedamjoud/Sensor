d4798ac2085063457f81ae1253ccefee1dab540c
formation <mohamedamjoud@outlook.com>
2023-02-18 01:58:53 +0100

    Update DummyTemperatureCaptor implementation

---
+++ Sensor/Adapter.TemperatureCaptor/DummyTemperatureCaptor.cs
@@ -0,0 +1,14 @@
+﻿using Core.SpiPort;
+using Core.UseCase;
+namespace Adapter.TemperatureCaptor;
+
+public class DummyTemperatureCaptor : ICaptorPort
+{
+    private const int EARTH_TEMPERATURE_MAX = 100;
+    private const int EARTH_TEMPERATURE_MIN = -100;
+    
+    public Task<int> GetTemperature()
+    {
+        return Task.FromResult(new Random().Next(EARTH_TEMPERATURE_MIN,EARTH_TEMPERATURE_MAX));
+    }
+}
