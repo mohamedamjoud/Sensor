b395be57b5e9d7ceae5782e2eacd5fd4c9d4fceb
formation <mohamedamjoud@outlook.com>
2023-02-22 06:44:00 +0100

    Update sensor state domain

--- Sensor/Adapter.SQLLit/Models/SensorState.cs
+++ Sensor/Adapter.SQLLit/Models/SensorState.cs
@@ -7,10 +7,14 @@ public class SensorState
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public sbyte Value { get; set; }
+    public int Value { get; set; }
     public DateTime DateTime { get; set; }
-    
-    public SensorState(sbyte value,DateTime dateTime)
+
+    public SensorState()
+    {
+        
+    }
+    public SensorState(int value,DateTime dateTime)
     {
         Value = value;
         DateTime = dateTime;
